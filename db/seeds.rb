1bdd7b5fe09986fd70e9a7dd6fe198286560dce4
Oksana <lorelsblog@gmail.com>
2015-12-05 14:06:18 +0200

    edit seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,13 @@ require 'googlebooks' # unless you're using Bundler
 results = GoogleBooks.search('ruby', :count => 30)
 puts "Total results for the term 'ruby': #{results.count}"
 for book in results
+
   @book = Book.create(title: book.title, isbn: book.isbn, lang: book.language,
                       published_date: book.published_date,
-                      description: book.description, remote_cover_url: book.image_link(:zoom => 2))
-  book.authors_array.each do |a|
-    author = Author.find_or_create_by(:name => a)
-    author.save
-    @book.authors << author
-  end
+                      description: book.description)
+  # book.authors_array.each do |a|
+  #   author = Author.find_or_create_by(:name => a)
+  #   author.save
+  #   @book.authors << author
+  # end
 end
