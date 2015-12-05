e99680755c875f302de489abc765c0f534350272
Oksana <lorelsblog@gmail.com>
2015-12-05 17:20:47 +0200

    add clear for col

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def scope_authors(tags)
+    links = []
+    if tags.count > 0
+      tags.each do |t|
+        links += [link_to(t.name, author_path(t))]
+      end
+      links.join(' &nbsp;&bull;&nbsp; ').html_safe
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :books
+  resources :authors
   root to: 'main#index'
   get '/auth/:provider/callback', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
