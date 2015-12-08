f0b893bc8abedf6f22ad278d77e57a7a695055c3
Oksana <lorelsblog@gmail.com>
2015-12-08 17:15:09 +0200

    add user controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :authors
   root to: 'books#index'
   # root to: 'main#index'
+  get 'users', to: 'users#index'
   get '/auth/:provider/callback', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 end
