cfc948bf3272c512da67b4582e562729ef02f426
Oksana <lorelsblog@gmail.com>
2015-12-15 16:58:50 +0200

    add cover to form

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -106,7 +106,7 @@ class BooksController < ApplicationController
     params.require(:isbn_form).permit(:isbn)
   end
   def book_params
-    params.require(:book).permit(:title, :isbn, :date_published, :lang, :description, :author_ids,
+    params.require(:book).permit(:title, :isbn, :date_published, :lang, :description, :author_ids, :cover, :cover_cache, :remove_cover,
                                  :category_ids => []
                                  )
   end
--- app/uploaders/cover_uploader.rb
+++ app/uploaders/cover_uploader.rb
@@ -10,6 +10,9 @@ class CoverUploader < CarrierWave::Uploader::Base
   version :thumb do
     process resize_to_fill: [200,200]
   end
+  version :preview do
+    process resize_to_fill: [50,50]
+  end
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
